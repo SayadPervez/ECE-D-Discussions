845d3271a43c90c799211d4d7ab695bbed6d4cfd
Glitch (rec-ece-d-discussions) <none>
2021-08-18 10:08:36 +0000

    Import of SayadPervez/ECE-D-Discussions

--- brain.js
+++ brain.js
@@ -2,6 +2,8 @@ window.onload = function(){
     func();
 }
 
+
+
 weekdays = {
     0 : "Monday",
     1 : "Monday",
@@ -12,12 +14,22 @@ weekdays = {
     6 : "Monday"
 }
 
+wdays = {
+    0 : "Sunday",
+    1 : "Monday",
+    2 : "Tuesday",
+    3 : "Wednesday",
+    4 : "Thursday",
+    5 : "Friday",
+    6 : "Saturday"  
+}
+
 timeList=['9:00 AM','10:15 AM',"11:30 AM","1:30 PM","2:45 PM"]
 
 function func()
 {
     var temp = new Date();
-    document.getElementById("nav-heading").innerText=`ECE D - ${weekdays[temp.getDay()]}`;
+    document.getElementById("nav-heading").innerText=`ECE D - ${wdays[temp.getDay()]}`;
     readJson((weekdays[temp.getDay()]).toLowerCase());
 }
 
@@ -39,6 +51,21 @@ function readJson (day) {
     })
  }
 
+ function red(x)
+ {
+     console.log("x:",x);
+     if(x=="Elective 2" || x=="Elective 1")
+     {
+        if(localStorage.getItem(x)==null)
+        {
+            window.location.replace("/settings.html")
+        }
+        return(localStorage.getItem(x));
+     }
+     else{
+         return(x);
+     }
+ }
 
 function doStuffJson(jOb)
 {
@@ -53,7 +80,7 @@ function doStuffJson(jOb)
     {
         htmlString+=`
         <div class="col s10 m10 l8 offset-s1 offset-m1 offset-l2">
-        <div class="card z-depth-4" onclick="window.location.assign('${jOb.links[periodList[i][0]]}')">
+        <div class="card z-depth-4" onclick="window.location.assign('${jOb.links[red(periodList[i][0])]}')">
         <div class="card-content white-text">
             <span class="card-title yellow-text text-darken-4" style="font-weight: 500;">${periodList[i][0]}
                 <p class="right-align" style="font-weight: 500;float:right">${timeList[i]}</p>
